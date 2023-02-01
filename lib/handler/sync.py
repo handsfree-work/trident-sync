69ff3042293cb4b562ca639aba343e97fcac5f59
xiaojunnuo <xiaojunnuo@qq.com>
2023-02-01 17:00:31 +0800

    fix: 同步前commit work repo

--- lib/handler/sync.py
+++ lib/handler/sync.py
@@ -100,10 +100,7 @@ class SyncHandler:
         logger.info(text_center("sync start"))
         config = self.config
         os.chdir(self.work_root)
-        # 先还原变更
-        logger.info("recovery files")
-        shell("git checkout .")
-        shell("git clean -f")
+        save_work_repo(self.repo, "save work repo before sync", self.config.options.push)
 
         # 如果work仓库配置了remote，先pull一下
         for remote in self.repo.remotes:
@@ -118,8 +115,12 @@ class SyncHandler:
         ref_count = sum(1 for ref in self.repo.refs)
         if ref_count > 0:
             # 判断这个仓库是否有过提交
-            shell(f"git submodule foreach git checkout .")
-            shell(f"git submodule foreach git clean -f")
+            try:
+                shell(f"git submodule foreach git checkout .")
+                shell(f"git submodule foreach git clean -f")
+            except Exception as e:
+                logger.warning('clean submodules failed')
+
             # 初始化一下子项目
             shell(f"git submodule update --init --recursive --progress")
             self.repo.iter_submodules()
